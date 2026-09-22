798932b9560c085151c2769bdc36308407895492
Arturo Chambi Villalobos <arturochambimv@gmail.com>
2026-09-22 19:48:24 -0400

    adicion de rutas en app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-"d"
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/cursos')
+def cursos():
+    return render_template('cursos.html')
+@app.route('/')
+def logout():
+    return f"Se cerro la sesion"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
