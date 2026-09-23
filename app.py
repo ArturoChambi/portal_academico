5edc2c295ecb67620edb43ffe3728db062e9f379
Arturo Chambi Villalobos <arturochambimv@gmail.com>
2026-09-22 20:03:49 -0400

    creacion de login

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,session,redirect,url_for
 
 app = Flask(__name__)
+
+usuarios = {
+"juan": "1234",
+"maria": "abcd",
+"pedro": "2026"
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    error = None
+
+    if request.method == 'POST':
+        username = request.form.get('username', '').strip()
+        password = request.form.get('password', '')
+
+        
+        if username in usuarios and usuarios[username] == password:
+           
+            session['usuario'] = username
+            
+            return redirect(url_for('cursos'))
+        else:
+            
+            error = "Usuario o contraseña incorrectos."
+
+    return render_template('login.html', error=error)
 @app.route('/cursos')
 def cursos():
     return render_template('cursos.html')
